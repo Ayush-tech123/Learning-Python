0ab56c919df275fb284eb64e25b0d2ebccad0bad
Ayush Sharma <ayush.sharma@avantika.edu.in>
2025-11-10 16:49:36 +0530

    Will upload rest from lecture 1 later

---
+++ Checking-Cases.py
@@ -0,0 +1,13 @@
+#strings
+
+import platform
+name = "ayush"
+
+print(name.upper().isupper())
+print(name.lower().isupper())
+print(name.upper().islower())
+print(name.lower().islower())
+
+#Finding length of a string
+
+print(len(name))
